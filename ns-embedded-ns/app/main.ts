e07d4117344347175bca53536919705840c6bc89
Jonathan Houze <jonathan.houze@gmail.com>
2018-07-03 16:08:54 +0800

    fix(angular) modify angular bootstrap to boot into an existing page

--- ns-embedded-ns/app/main.ts
+++ ns-embedded-ns/app/main.ts
@@ -1,5 +1,11 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+
+import { enableProdMode } from "@angular/core";
+import { AppOptions } from "nativescript-angular/platform-common";
+import { APP_ROOT_VIEW } from "nativescript-angular/platform-providers";
+import { run } from "tns-core-modules/application";
+import { GridLayout } from "tns-core-modules/ui/layouts/grid-layout";
 import { AppModule } from "./app.module";
 
 /* ***********************************************************
@@ -9,4 +15,23 @@ import { AppModule } from "./app.module";
 *************************************************************/
 import "./shared/kinvey.common";
 
-platformNativeScriptDynamic().bootstrapModule(AppModule);
+run({
+  create: () => {
+    // Create root view for the application.
+    const root = new GridLayout();
+
+    const appOptions: AppOptions = {
+      startPageActionBarHidden: true,
+      bootInExistingPage: true
+    };
+
+    const extraProviders = [{ provide: APP_ROOT_VIEW, useValue: root }];
+
+    enableProdMode();
+
+    platformNativeScriptDynamic(appOptions, extraProviders)
+      .bootstrapModule(AppModule);
+
+    return root;
+  }
+});
